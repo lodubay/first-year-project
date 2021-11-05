224d59bc5f6aac1c73845d1b1c618dcb9a84c771
Liam Dubay <liam.dubay@gmail.com>
2021-11-05 09:28:31 -0400

    started age comparison

--- compare_datasets.py
+++ compare_datasets.py
@@ -23,14 +23,13 @@ def decode(df):
     return df
 
 ### APOKASC catalog
+print('Importing APOKASC catalog...')
 data = Table.read(data_path / apokasc_file, format='fits')
 apokasc_df = decode(data.to_pandas())
-# data, hdr = getdata(data_path / apokasc_file, 1, header=True)
-# apokasc_df = pd.DataFrame(data)
-# print(apokasc_df)
 
 # Relevant data columns
 id_col = '2MASS_ID'
+loc_col = 'LOC_ID'
 alpha_col = 'DR16_ALP_M_COR'
 alpha_err_col = 'ALP_M_COR_ERR'
 fe_col = 'DR16_FE_H'
@@ -41,29 +40,40 @@ age_col = 'APOKASC2_AGE'
 age_err1_col = 'APOKASC2_AGE_MERR' # error in negative direction
 age_err2_col = 'APOKASC2_AGE_PERR' # error in positive direction
 
+# DataFrame with only ages
 ages = apokasc_df[apokasc_df[age_col] > 0]
-ages = ages[[id_col, age_col, age_err1_col, age_err2_col]]
-ages.columns = ['id', 'apokasc2_age', 'apokask2_age_err1', 'apokask2_age_err2']
-ages = ages.set_index('id')
-print(ages)
-print(ages.index.dtype)
+ages = ages[[id_col, loc_col, age_col, age_err1_col, age_err2_col]]
+ages.columns = ['id', 'loc_id', 'apokasc2_age', 'apokask2_age_err1', 
+                'apokask2_age_err2']
+ages['loc_id'] = ages['loc_id'].astype(int)
+ages.set_index(['id', 'loc_id'], inplace=True)
 
 ### astroNN DR17 catalog
+print('Importing astroNN DR17 catalog...')
 data = Table.read(data_path / astroNN_file, format='fits')
 astroNN_df = decode(data.to_pandas())
 
 id_col = 'APOGEE_ID'
+loc_col = 'LOCATION_ID'
 age_col = 'age_lowess_correct'
 age_err_col = 'age_total_error'
 fe_col = 'FE_H'
 fe_err_col = 'FE_H_ERR'
 
-astroNN_ages = astroNN_df[astroNN_df[age_col] > 0][[id_col, age_col, age_err_col]]
-ages = ages.join(astroNN_ages.set_index(id_col), how='inner')
-ages = ages.rename(columns={'age_lowess_correct': 'astroNN_age', 
-                            'age_total_error': 'astroNN_age_err'})
-ages.drop_duplicates(inplace=True)
+astroNN_ages = astroNN_df[astroNN_df[age_col] > 0]
+astroNN_ages = astroNN_ages[[id_col, loc_col, age_col, age_err_col]]
+astroNN_ages.columns = ['id', 'loc_id', 'astroNN_age', 'astroNN_age_err']
+astroNN_ages['loc_id'] = astroNN_ages['loc_id'].astype(int)
+astroNN_ages.set_index(['id', 'loc_id'], inplace=True)
+ages = ages.join(astroNN_ages, how='inner')
 print(ages)
+# ages = ages.rename(columns={'age_lowess_correct': 'astroNN_age', 
+#                             'age_total_error': 'astroNN_age_err'})
+# print(ages)
+
+# for i, id in enumerate(ages.index):
+#     if id in ages.index[:i]:
+#         print(ages.loc[id, ['apokasc2_age', 'astroNN_age', 'astroNN_age_err']])
 
 # astroNN_ages = data[data[age_col] > 0][[id_col, age_col, age_err_col]]
 # astroNN_ages.rename_column(id_col, 'ID')
